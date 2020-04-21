00d05893fccb465e0357508104e9c5cf2fe6de60
Jesús Redondo García <redondogarciajesus@gmail.com>
2020-04-21 12:43:21 +0200

    Battle line V1

---
+++ games/BattleLine/precache-manifest.7e8444cf07eeb55e623174af8752b1be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab71982a6f258a753fb1722708144b07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0442dc3fd76a72c71e12",
+    "url": "/static/css/main.1fa582d9.chunk.css"
+  },
+  {
+    "revision": "853a21f5e1f55d1c9df3",
+    "url": "/static/js/2.5d20c5a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d20c5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0442dc3fd76a72c71e12",
+    "url": "/static/js/main.9355aade.chunk.js"
+  },
+  {
+    "revision": "6aa3b5d2173b85651b1e",
+    "url": "/static/js/runtime-main.6da7face.js"
+  },
+  {
+    "revision": "17e71f1698620d532ea5c966edf1434d",
+    "url": "/static/media/Red_flag.17e71f16.png"
+  },
+  {
+    "revision": "8698b6a0b143f311d361a5a22bb072f0",
+    "url": "/static/media/card_back.8698b6a0.png"
+  }
+]);
